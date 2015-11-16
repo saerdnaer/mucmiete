b8cf10aede1b0642e88f910f96c13ded46fe3445
Thomas Tanner <tanner@gmx.net>
2015-11-16 01:22:02 +0100

    minimal form, add email and ipaddress, admin optional

---
+++ miete/forms.py
@@ -0,0 +1,29 @@
+    # -*- coding: utf-8 -*-
+
+from django.forms import ModelForm, HiddenInput
+from captcha.fields import ReCaptchaField
+from .models import Miete
+
+class MieteForm(ModelForm):
+    class Meta:
+        model = Miete
+        fields = ('kaltmiete','groesse','plz','stadtteil',
+                'bewohner','abschluss','erhoehung','vermieter',
+                'email','ipaddress')
+        widgets = {'ipaddress': HiddenInput()}
+        help_texts = {
+            'kaltmiete':'Kaltmiete ist die Grundmiete ohne Nebenkosten wie Wasser,Heizung,Strom,Telefon etc.',
+            'groesse':'Die Wohnungsgröße in Quadratmetern. Dachschrägen...',
+            'plz':'Die Postleitzahl in München 8xxxx',
+            'stadtteil':'Der Stadtteil',
+            'bewohner':'Anzahl aller Bewohner in der Wohnung',
+            'abschluss':'Jahr des Abschlusses des Mietvertrags',
+            'erhoehung':'Jahr der letzten Mieterhöhung',
+            'vermieter':'Art des Vermieters',
+            'email':'Benachrichtung über Ergebnis der Umfrage',
+        }
+    captcha = ReCaptchaField(attrs={'theme' : 'clean'})
+    
+    def __init__(self, *args, **kwargs):
+        super(MieteForm, self).__init__(*args, **kwargs)
+        #self.fields['email'].required = False
