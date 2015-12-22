1f24165b9a5ac048d723a1772d52e51495c08b7c
konstin <konstin@wmail.io>
2015-12-22 22:24:29 +0100

    Einstellungen

--- settings/production.py
+++ settings/production.py
@@ -5,22 +5,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '::1']
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
 
 HTTPS_SUPPORT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-#STATIC_ROOT = "/var/www/example.com/static/"
