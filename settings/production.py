697dd1c5a484744f535ca2e4aebe88eee712f77d
konstin <konstin@wmail.io>
2015-12-22 22:14:55 +0100

    Einstellungsdateien verbessern

--- settings/production.py
+++ settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '::1']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
