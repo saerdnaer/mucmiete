3183b3624ada88ae6b9ed0907d8ebc4f01848f63
konstin <konstin@wmail.io>
2015-12-25 15:01:12 +0100

    add a footer with example content that stays at the bottom of the page

--- miete/views.py
+++ miete/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+
 from ipware.ip import get_ip
+
 from .forms import MieteForm
 from .models import Email
 
@@ -25,6 +27,6 @@ def index_view(request, **kwargs):
     
     # TODO: store both lists in the database
     stadtbezirke   = ["Allach", "Untermenzing", "Altstadt", "Lehel", "Au", "Haidhausen", "Aubing", "Lochhausen", "Langwied", "Berg am Laim", "Bogenhausen", "Feldmoching", "Hasenbergl", "Hadern", "Laim", "Ludwigsvorstadt", "Isarvorstadt", "Maxvorstadt", "Milbertshofen", "Am Hart", "Moosach", "Neuhausen", "Nymphenburg", "Obergiesing", "Pasing", "Obermenzing", "Ramersdorf", "Perlach", "Schwabing", "Freimann", "Schwabing-West", "Schwanthalerhöhe", "Sendling", "Sendling-Westpark", "Thalkirchen", "Obersendling", "Fürstenried", "Forstenried", "Solln", "Trudering", "Riem", "Untergiesing", "Harlaching"]
-    postleitzahl = ["80331", "80333", "80335", "80336", "80337", "80339", "80469", "80538", "80539", "80634", "80636", "80637", "80638", "80639", "80686", "80687", "80689", "80796", "80797", "80798", "80799", "80801", "80802", "80803", "80804", "80805", "80807", "80809", "80933", "80935", "80937", "80939", "80992", "80993", "80995", "80997", "80999", "81241", "81243", "81245", "81247", "81249", "81369", "81371", "81373", "81375", "81377", "81379", "81475", "81476", "81477", "81479", "81539", "81541", "81543", "81545", "81547", "81549", "81667", "81669", "81671", "81673", "81675", "81677", "81679", "81735", "81737", "81739", "81825", "81827", "81829", "81925", "81927", "81929"]
+    postleitzahlen = ["80331", "80333", "80335", "80336", "80337", "80339", "80469", "80538", "80539", "80634", "80636", "80637", "80638", "80639", "80686", "80687", "80689", "80796", "80797", "80798", "80799", "80801", "80802", "80803", "80804", "80805", "80807", "80809", "80933", "80935", "80937", "80939", "80992", "80993", "80995", "80997", "80999", "81241", "81243", "81245", "81247", "81249", "81369", "81371", "81373", "81375", "81377", "81379", "81475", "81476", "81477", "81479", "81539", "81541", "81543", "81545", "81547", "81549", "81667", "81669", "81671", "81673", "81675", "81677", "81679", "81735", "81737", "81739", "81825", "81827", "81829", "81925", "81927", "81929"]
     
-    return render(request, 'index.html', {'form': form, 'stadtbezirke': stadtbezirke, 'postleitzahl': postleitzahl})
+    return render(request, 'index.html', {'form': form, 'stadtbezirke': stadtbezirke, 'postleitzahlen': postleitzahlen})
