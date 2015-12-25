5832327cd7cac239dc49e11721edbaac95842e92
konstin <konstin@wmail.io>
2015-12-25 19:21:29 +0100

    Add the necessary pages to make the footer working

--- settings/urls.py
+++ settings/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.conf import settings
+from django.views.generic import TemplateView
+
 from miete.views import index_view
 
 urlpatterns = [
     url(r'^$', index_view, name='index'),
+    url(r'^about/?$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^datenschutz/?$', TemplateView.as_view(template_name="datenschutz.html"), name="datenschutz"),
+    url(r'^impressum/?$', TemplateView.as_view(template_name="impressum.html"), name="impressum"),
 ]
+
 if settings.HAVE_ADMIN:
     from django.contrib import admin
     from django.contrib.auth import views as auth_views
