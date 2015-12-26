b2c1b8142288086326f4a7a4a05dcc7eb9c672fd
konstin <konstin@wmail.io>
2015-12-27 00:17:30 +0100

    save state that is kind of working

--- miete/forms.py
+++ miete/forms.py
@@ -6,17 +6,24 @@ from captcha.fields import ReCaptchaField
 from .models import Miete
 
 
-class MieteForm(ModelForm):
-
+class MieteFormPlicht(ModelForm):
     class Meta:
         model = Miete
-        fields = ('kaltmiete', 'groesse', 'plz', 'stadtbezirk',
-                  'bewohner', 'abschluss', 'erhoehung', 'vermieter')
+        fields = ('kaltmiete', 'groesse', 'plz', 'stadtbezirk')
         help_texts = {
             'kaltmiete': 'Deine Miete ohne Nebenkosten wie Strom, Wasser, Heizung, Hausverwaltung, Garage, etc.',
             'groesse': 'Die Wohnungsgröße in Quadratmetern wie sie im Mietvertrag festgelegt wurde.',
             'plz': 'Deine Postleitzahl',
             'stadtbezirk': 'Der stadtbezirk in dem du wohnst.',
+        }
+    captcha = ReCaptchaField(attrs={'theme': 'clean'})
+
+
+class MieteFormOptional(ModelForm):
+    class Meta:
+        model = Miete
+        fields = ('bewohner', 'abschluss', 'erhoehung', 'vermieter')
+        help_texts = {
             'bewohner': 'Die Anzahl aller Bewohner in der Wohnung',
             'abschluss': 'Das Jahr des Abschlusses des Mietvertrags',
             'erhoehung': 'Das Jahr der letzten Mieterhöhung',
@@ -25,4 +32,3 @@ class MieteForm(ModelForm):
     
     email = EmailField(required=False, label='E-Mail',
                        help_text='Trage deine E-Mailadresse hier ein, wenn Du über das Ergebnis der Umfrage benachrichtigt werden willst')
-    captcha = ReCaptchaField(attrs={'theme': 'clean'})
