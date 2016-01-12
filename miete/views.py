ee7e60a7e011cc93f691f8687e33d2006209e4a7
konstin <konstin@wmail.io>
2016-01-12 17:21:06 +0100

    small fixes

--- miete/views.py
+++ miete/views.py
@@ -42,10 +42,10 @@ class Umfrage(SessionWizardView):
         context["stadtbezirke"]   = ['Allach', 'Altstadt', 'Am Hart', 'Au', 'Aubing', 'Berg am Laim', 'Bogenhausen', 'Feldmoching', 'Forstenried', 'Freimann', 'Fürstenried', 'Hadern', 'Haidhausen', 'Harlaching', 'Hasenbergl', 'Isarvorstadt', 'Laim', 'Langwied', 'Lehel', 'Lochhausen', 'Ludwigsvorstadt', 'Maxvorstadt', 'Milbertshofen', 'Moosach', 'Neuhausen', 'Nymphenburg', 'Obergiesing', 'Obermenzing', 'Obersendling', 'Pasing', 'Perlach', 'Ramersdorf', 'Riem', 'Schwabing', 'Schwabing-West', 'Schwanthalerhöhe', 'Sendling', 'Sendling-Westpark', 'Solln', 'Thalkirchen', 'Trudering', 'Untergiesing', 'Untermenzing']
         context["postleitzahlen"] = ["80331", "80333", "80335", "80336", "80337", "80339", "80469", "80538", "80539", "80634", "80636", "80637", "80638", "80639", "80686", "80687", "80689", "80796", "80797", "80798", "80799", "80801", "80802", "80803", "80804", "80805", "80807", "80809", "80933", "80935", "80937", "80939", "80992", "80993", "80995", "80997", "80999", "81241", "81243", "81245", "81247", "81249", "81369", "81371", "81373", "81375", "81377", "81379", "81475", "81476", "81477", "81479", "81539", "81541", "81543", "81545", "81547", "81549", "81667", "81669", "81671", "81673", "81675", "81677", "81679", "81735", "81737", "81739", "81825", "81827", "81829", "81925", "81927", "81929"]
         
-        context["is_embeded"] = "embed" in self.request.GET
+        context["is_embedded"] = "embed" in self.request.GET
         
         return render(self.request, self.get_template_names(), context)
     
     def done(self, form_list, **kwargs):
         self.save_model()
-        return render(self.request, "done.html", {"is_embeded": "embed" in self.request.GET})
+        return render(self.request, "done.html", {"is_embedded": "embed" in self.request.GET})
