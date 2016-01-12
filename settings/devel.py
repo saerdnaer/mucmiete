dcd4933922e017be43050ebabb97be0d510aadb8
konstin <konstin@wmail.io>
2016-01-12 17:21:06 +0100

    small fixes

--- settings/devel.py
+++ settings/devel.py
@@ -6,7 +6,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '::1']
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
